c8784cc43ce036045223ec30d0ae7186fedccb5f
Christian David Ospina Primero <christianprimero26@hotmail.com>
2023-02-13 00:46:47 -0500

    Update SpringBootReactorApplication.java
    8. El método subscribe()
    Manejo de error con expresiones lambda

--- src/main/java/com/cospina/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/cospina/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,7 @@
 package com.cospina.springboot.reactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +11,8 @@ import reactor.core.publisher.Flux;
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner{
 
+	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
 	}
@@ -16,10 +20,17 @@ public class SpringBootReactorApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Flux<String> names = Flux.just("Andres", "Pedro", "Diego", "Juan")
-				.doOnNext(System.out::println);		
+		Flux<String> names = Flux.just("Andres", "Pedro", "", "Juan")
+				.doOnNext(e -> { 
+					if (e.isEmpty()) {
+						throw new RuntimeException("Nombres no pueden ser vacios");
+					} else {
+						System.out.println(e);
+					}	
+				});		
 		
-		names.subscribe();
+		names.subscribe(e -> log.info(e),
+				error -> log.error(error.getMessage()));
 	}
 
 }
