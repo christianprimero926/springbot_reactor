b93625b40922a1e5cb19ed366c747fe9d0f635d0
Christian David Ospina Primero <christianprimero26@hotmail.com>
2023-02-13 00:28:49 -0500

    Update SpringBootReactorApplication.java
    7. Creando un Stream Flux Observable

--- src/main/java/com/cospina/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/cospina/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,13 +1,25 @@
 package com.cospina.springboot.reactor.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import reactor.core.publisher.Flux;
+
 @SpringBootApplication
-public class SpringBootReactorApplication {
+public class SpringBootReactorApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Flux<String> names = Flux.just("Andres", "Pedro", "Diego", "Juan")
+				.doOnNext(System.out::println);		
+		
+		names.subscribe();
 	}
 
 }
