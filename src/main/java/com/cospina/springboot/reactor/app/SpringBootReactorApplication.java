d775703415eb3e31e8b05af93dbe6a82c956280d
Christian David Ospina Primero <christianprimero26@hotmail.com>
2023-02-13 18:04:08 -0500

    Seccion 2
    9. El evento onComplete

--- src/main/java/com/cospina/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/cospina/springboot/reactor/app/SpringBootReactorApplication.java
@@ -9,28 +9,32 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class SpringBootReactorApplication implements CommandLineRunner{
+public class SpringBootReactorApplication implements CommandLineRunner {
 
 	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		Flux<String> names = Flux.just("Andres", "Pedro", "", "Juan")
-				.doOnNext(e -> { 
-					if (e.isEmpty()) {
-						throw new RuntimeException("Nombres no pueden ser vacios");
-					} else {
-						System.out.println(e);
-					}	
-				});		
-		
-		names.subscribe(e -> log.info(e),
-				error -> log.error(error.getMessage()));
+
+		Flux<String> names = Flux.just("Andres", "Pedro", "Maria", "Juan").doOnNext(e -> {
+			if (e.isEmpty()) {
+				throw new RuntimeException("Nombres no pueden ser vacios");
+			} else {
+				System.out.println(e);
+			}
+		});
+
+		names.subscribe(e -> log.info(e), error -> log.error(error.getMessage()), new Runnable() {
+
+			@Override
+			public void run() {
+				log.info("Ha finalizado la ejecucion del observable con exito");
+			}
+		});
 	}
 
 }
